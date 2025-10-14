57377e6960b1d58e1554a22476be0eb7a2f83121
Ubuntu <ubuntu@ip-172-31-16-186.ec2.internal>
2025-10-14 05:21:20 +0000

    Trrigger 3

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 5000;
+const port = 8080;
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
